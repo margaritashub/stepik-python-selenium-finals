ca19a36c0ab551d06cf15e0256513ff84529e3cc
Rita <60928926+margaritashub@users.noreply.github.com>
2020-03-08 23:37:20 +0500

    base page added

--- test_main_page.py
+++ test_main_page.py
@@ -3,3 +3,11 @@ def test_guest_can_go_to_login_page(browser):
     browser.get(link)
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
+
+def go_to_login_page(browser):
+    link = browser.find_element_by_css_selector("#login_link")
+    link.click()
+
+def test_guest_can_go_to_login_page(browser):
+   browser.get(link)
+   go_to_login_page(browser)
