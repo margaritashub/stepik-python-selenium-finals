bf31d28b5ebe4b05142030e4c29e8d43fdf20ba7
Rita <60928926+margaritashub@users.noreply.github.com>
2020-03-09 20:40:40 +0500

    login page added

---
+++ pages1/login_page.py
@@ -0,0 +1,24 @@
+from .base_page import BasePage
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        current_url = self.browser.current_url
+        assert "login" in current_url, f"No login substring in the {current_url}"l
+        assert True
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+        assert True
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+        assert True
